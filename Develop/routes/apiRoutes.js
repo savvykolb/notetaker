1815de949248ac2a03fa9c36e3ddc10ea7ba6bd0
Savannah Kolb <savannahnkolb@gmail.com>
2021-06-12 22:42:29 -0400

    I was able to get UUID to work

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,6 +1,7 @@
 
 const fs = require("fs");
 const notesData = require('../db/db.json');
+const { v4: uuidv4 } = require('uuid');
 
 // ROUTING
 
@@ -11,7 +12,10 @@ module.exports = (app) => {
   // API POST Requests
 
   app.post("/api/notes", (req, res) => {
+    req.body.uuidv4 = uuidv4();
       notesData.push(req.body);
+      // console.log('LOOOOOOOOOKKKKKKKKKKKKK', req.body)
+      // console.log('UUID', req.body)
       fs.writeFile('./db/db.json', JSON.stringify(notesData), (err) => {
         if (err) throw err;
         console.log('Wrote Data!!!!');
