1adfed5833dd7d709127fa9eae005ab7bb89a15f
Savannah Kolb <savannahnkolb@gmail.com>
2021-06-12 23:21:59 -0400

    psuedo code for delete button

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -12,9 +12,9 @@ module.exports = (app) => {
   // API POST Requests
 
   app.post("/api/notes", (req, res) => {
-    req.body.uuidv4 = uuidv4();
+    req.body.id = uuidv4();
       notesData.push(req.body);
-      // console.log('LOOOOOOOOOKKKKKKKKKKKKK', req.body)
+      console.log('LOOOOOOOOOKKKKKKKKKKKKK', req.body)
       // console.log('UUID', req.body)
       fs.writeFile('./db/db.json', JSON.stringify(notesData), (err) => {
         if (err) throw err;
@@ -23,3 +23,8 @@ module.exports = (app) => {
       });
 
 })};
+
+
+//app.delete uses UUID ${ID} to delete - see line 47 on apiRoutes
+//I need a function that loops OR map through the array until it matches a UUID to delete.
+// Then I should be able to use same write file from app.post
