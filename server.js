d0516e9e57f7d6f32226aff347929a164c59a88e
Savannah Kolb <savannahnkolb@gmail.com>
2021-06-13 20:36:49 -0400

    changed file structure for Heroku

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ app.use(express.static("public"));
 // The below points our server to a series of "route" files.
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 
-require('./routes/apiRoutes')(app);
-require('./routes/htmlRoutes')(app);
+require('./Develop/routes/apiRoutes')(app);
+require('./Develop/routes/htmlRoutes')(app);
 
 // LISTENER
 // The below code effectively "starts" our server
