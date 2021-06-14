7a917eb2710eaf0ceeb3da55ca0aeca59efb746a
Savannah Kolb <savannahnkolb@gmail.com>
2021-06-13 22:44:26 -0400

    broken code from moving files sshould be working now

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -6,39 +6,35 @@ const { v4: uuidv4 } = require('uuid');
 // ROUTING
 
 module.exports = (app) => {
-
   app.get("/api/notes", (req, res) => res.json(notesData));
-
   // API POST Requests
 
   app.post("/api/notes", (req, res) => {
-    req.body.id = uuidv4();
+    req.body.uuidv4 = uuidv4();
       notesData.push(req.body);
       console.log('LOOOOOOOOOKKKKKKKKKKKKK', req.body)
-      // console.log('UUID', req.body)
-      fs.writeFile('./db/db.json', JSON.stringify(notesData), (err) => {
+      
+      fs.writeFile('./Develop/db/db.json', JSON.stringify(notesData), (err) => {
         if (err) throw err;
         console.log('Wrote Data!!!!');
         res.json(notesData);
       });
+})};
 
-      
+//   app.delete("/api/notes/:id", (req, res) => {
+//     const deletedUUID = req.params.id
+//     console.log('req.params.id:', req.params.id)
+//     let notesChanged = require('../db/db.json'); // needed to require here with let for changing json file
+//     for (let index = 0; index < notesChanged.length; index++) {
+//       if (notesChanged[index].id === deletedUUID);
+//     }
+//     // fs.writeFile('./db/db.json', JSON.stringify(notesChanged), (err) => {
+//     //   if (err) throw err;
+//     //   console.log('Wrote Data!!!!');
+//     //   res.json(notesChanged);
+//   })
 
-})
-  app.delete("/api/notes/:id", (req, res) => {
-    const deletedUUID = req.params.id
-    for (let index = 0; index < notesData.length; index++) {
-      if (notesData[index].id === deletedUUID);
-      console.log('----------deletedUUID:', deletedUUID)
-    }
-    fs.writeFile('./db/db.json', JSON.stringify(notesData), (err) => {
-      if (err) throw err;
-      console.log('Wrote Data!!!!');
-      res.json(notesData);
-  })
-
-});
-}
+// ;
 
 
 // app.delete uses UUID ${ID} to delete - see line 47 on apiRoutes
